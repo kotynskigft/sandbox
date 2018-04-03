ea0e7ee3149157e86f028b2088f80a24a25c9047
Slawomir Siudek <siudeks@gmail.com>
2018-04-03 22:52:50 +0200

    Finished example assignment

--- progfun2/src/main/scala/example/Lists.scala
+++ progfun2/src/main/scala/example/Lists.scala
@@ -23,7 +23,14 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
+    def sum(xs: List[Int]): Int = {
+      def sumint(l: List[Int], acc: Int): Int = l match {
+        case Nil => acc
+        case head::tail => sumint(tail, acc + head)
+      }
+      sumint(xs, 0)
+    }
+    
   
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +45,17 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      if (xs.isEmpty) throw new NoSuchElementException()
+      val first = xs.head
+      def maxint(t: List[Int], result: Int): Int = t match {
+        case Nil => result
+        case head :: tail => 
+          if (head > result)
+            head
+          else
+            result
+      }
+      maxint(xs.tail, first)
+    }
   }
